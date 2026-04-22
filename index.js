cd84e11d056bd638418197cbf1ce92af35e20942
Rahul <rahultomarjobs6296@gmail.com>
2026-04-22 08:02:55 +0530

    Change the port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import axios from "axios";
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
